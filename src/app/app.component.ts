bb21ccd96a1159b37ae36bf7a7b9234a95301df1
Rachel W. <rachelwilliams@mailbox.org>
2024-01-25 09:52:42 +0100

    first day of playing with angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { TaskListComponent } from './task-list/task-list.component'
+
+export interface TaskInterface {
+  id: number;
+  name: string;
+  done: boolean;
+  comment?: string;
+}
 
 @Component({
   selector: 'digi-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, CommonModule, TaskListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'todolist';
+  test = { text: "Buddy is the best" };
+  hello = "hello";
+  tasks: TaskInterface[] = [{ id: 1, name: 'Faire la vaisselle', done: false, comment: "Dépêche toi mon lapin, je ne supporte pas de voir traîner la vaisselle" }, { id: 2, name: 'Faire le ménage', done: false }, { id: 3, name: "Dormir", comment: "Important!", done: true }];
+  logName = (givenname: string) => {
+    console.log(givenname)
+  }
+  onButtonValidate(task: TaskInterface): void {
+    task.done = !task.done;
+  };
+  // console.log("bouton validé cliqué", taskId);
 }
+
---
+++ src/app/task-list/task-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { TaskInterface } from '../shared/interfaces/task.interface';
+@Component({
+  selector: 'digi-task-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './task-list.component.html',
+  styleUrl: './task-list.component.css'
+})
+export class TaskListComponent {
+
+}
